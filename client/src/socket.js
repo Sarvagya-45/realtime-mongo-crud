8af3a67caa3798e6f19bb1ec6a5d28cd7b292af8
Sarvagya Shukla <sarvagyashukla28@gmail.com>
2026-08-16 17:59:29 +0530

    Update socket client

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,4 +2,12 @@ import { io } from "socket.io-client";
 
 const socket = io("https://mongo-crud-app-lhgo.onrender.com");
 
+socket.on("connect", () => {
+  console.log("✅ Socket Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("❌ Socket Error:", err.message);
+});
+
 export default socket;
